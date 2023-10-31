775e3b4861ce2be0c2344afa47b55cc5f28862a3
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2023-10-31 10:31:11 -0300

    feat - implements logout button

--- quizhero-app/apps/quizzes/urls.py
+++ quizhero-app/apps/quizzes/urls.py
@@ -1,10 +1,13 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
-from . import views
 
+from . import views
 
 app_name = "quizzes"
 urlpatterns = [
     path("", views.home, name="home"),
     path('quiz/<int:id>/', views.quiz, name="quiz"),
     path('login/', views.MyLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
