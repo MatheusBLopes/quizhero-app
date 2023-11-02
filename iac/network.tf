ab8bac46f33e3a295ab7a8fece651870ed6fc885
Matheus Bachiste Lopes <matheusblopes10@gmail.com>
2023-11-02 06:56:08 -0300

    feat: update iac files

---
+++ iac/network.tf
@@ -0,0 +1,82 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "172.31.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "MainVpc"
+  }
+}
+
+# Create Internet Gateway and associate with the VPC
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a subnet (you can replicate this block for other subnets)
+resource "aws_subnet" "subnet_a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "172.31.32.0/20" # Specify CIDR block for the subnet, e.g., "172.31.1.0/24"
+  availability_zone = "us-east-1a" # For other subnets, change this to respective AZs
+
+  tags = {
+    Name = "MySubnetA"
+  }
+}
+
+# Main route table to associate with the VPC
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "MainRouteTable"
+  }
+}
+
+# Associate main route table with the subnet
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet_a.id
+  route_table_id = aws_route_table.main.id
+}
+
+
+resource "aws_security_group" "allow_all" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all"
+  }
+}
+
+resource "aws_eip" "quizhero_ip" {
+  instance = aws_instance.quizhero_instance.id
+
+  depends_on = [ aws_instance.quizhero_instance ]
+}
