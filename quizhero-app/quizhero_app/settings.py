61742c74574ac559b6058d619d50337d2c6fde68
MatheusBLopes <matheusblopes10@gmail.com>
2023-12-25 14:45:52 -0300

    feat: update db schema

--- quizhero-app/quizhero_app/settings.py
+++ quizhero-app/quizhero_app/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'options': '-c search_path=quizhero_schema'
+        },
     }
 }
 
