3215e0c4faf790f88ebcb5e249a3bb360302284b
MatheusBLopes <matheusblopes10@gmail.com>
2023-12-25 11:44:16 -0300

    update settings

--- quizhero-app/quizhero_app/settings.py
+++ quizhero-app/quizhero_app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import ast
+import os
 from pathlib import Path
 
 from prettyconf import config
@@ -85,9 +86,13 @@ WSGI_APPLICATION = 'quizhero_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
